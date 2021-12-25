0058e7396a7ecfe5c50fd402debe501a19724df6
Rafi5734 <kamrulhasun5734@gmail.com>
2021-12-25 23:17:25 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/Head";
+import Header from "../Components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Hulu 2.0</title>
+            </Head>
+
+            {/* Header Start */}
+            <Header></Header>
+            {/* Header End*/}
+
+            {/* Navbar Start */}
+
+            {/* Navbar End */}
+
+            {/* Result Start */}
+
+            {/* Result End */}
+        </div>
+    );
+}
